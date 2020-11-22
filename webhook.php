082ae3d4a3b34e6b067e31b763abdb4bc0b054bd
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-11-23 02:18:28 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -14,11 +14,7 @@ foreach ($request_json['events'] as $event)
 		{
 			$text = $event['message']['text'];
 			
-			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';   
-			
-			$reply_message = "KANT";
-			
-			$reply_message = mySQL_selectAll('http://s61160080.kantit.com/json_select.php');
+			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!'; 
 			
 		} else {
 			$reply_message = 'ฉันได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
