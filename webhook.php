9440bc35a20a5fcf13f3544193af9161a795413a
supaponR <78609220+supaponR@users.noreply.github.com>
2021-10-27 15:56:12 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,6 +1,6 @@
 <?php
 
-$channelAccessToken = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$channelAccessToken = 'yQ6jcvVUPxlGbi3bmUc71nxZkkHjJ7eSAsMjKAsNgshbU3RvAHC+nty5XMYlEBhUoEX9sFqeej2lxVMBQ7zwh8CKiIS6HM8f2wFH0YTePQHktSkIaNZbsSl+n2Yoa2QwhIOc6z6u4MHLzILbInKTYwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 
 $request = file_get_contents('php://input');   // Get request content
 
