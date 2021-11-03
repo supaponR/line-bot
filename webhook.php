957c575ac91624b860f55ce81a4dc421323f3c91
supaponR <78609220+supaponR@users.noreply.github.com>
2021-11-03 14:43:45 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -20,7 +20,7 @@ foreach ($request_json['events'] as $event)
 				$reply_message = " ผู้พัฒนาระบบคือ น.ส สุภาพร ระเบียบโพธิ์";
 			}
 			if("เส้นทางไปที่มหาลัยKMUTT" == $text || "ขอLinkMapไปKmutt "  == $text || "ขอLinkเส้นทางไปKMUTT"  == $text){
-				$reply_message = " https://goo.gl/maps/D2sFxAPfZdCfkvcRA " ;
+				$reply_message = "https://goo.gl/maps/D2sFxAPfZdCfkvcRA " ;
 			}
 			if("ขอภาพเส้นทางไปKMUTT "){
 				$reply_message ="https://drive.google.com/file/d/1Pt0-iopFx3ji-THqOAa4H-HN72YX41Ag/view?usp=sharing";
