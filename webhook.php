922dc99755543416eeaae507e06cfd85d0c6b90a
supaponR <78609220+supaponR@users.noreply.github.com>
2021-11-03 15:02:08 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -23,7 +23,7 @@ foreach ($request_json['events'] as $event)
 				$reply_message = "https://goo.gl/maps/D2sFxAPfZdCfkvcRA" ;
 			}
 			if("ขอภาพเส้นทางไปKMUTT" == $text){
-				$reply_message ="https://drive.google.com/file/d/1Pt0-iopFx3ji-THqOAa4H-HN72YX41Ag/view?usp=sharing";
+				$reply_message = "https://s115.convertio.me/p/-o3hBt8xVJZtIFAj1zk0Kg/e0887ef6a79110ea2d460c25d9ea94db/B211223D-58E3-4B02-858D-EDEBC4B8BAAE.webp" ;
 			}
 			if(("covid-19" == $text) || ("Covid-19" == $text) || ("สถานการณ์โควิด" == $text) || ("สถานการณ์โควิดวันนี้" == $text) ){
 				$reply_message = "เสียชีวิตเพิ่มขึ้น!!";
